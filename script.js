8081618edfbdf7f2a7297232ed08facfd4e6565a
Saud Zahir <76210541+aljabrak@users.noreply.github.com>
2021-10-13 18:23:41 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,21 @@
+function quote(){
+    return "If you can't explain it simply you don't understand it well enough."
+};
+
 function youtube()
 {
     document.getElementById("youtubelink").style.display = 'block';
-}
+};
 
 var trailer = document.getElementById("trailer");
 
 function trailerplay(){
     trailer.play(); 
-}
+};
 
 function pause_video(){ 
     trailer.pause();
-}
+};
+
+
+
