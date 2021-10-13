acbaba03383fe23257ad2a0e95d6c651200f442e
Saud Zahir <76210541+aljabrak@users.noreply.github.com>
2021-10-13 18:03:33 +0500

    Update script.js

--- script.js
+++ script.js
@@ -3,4 +3,12 @@ function youtube()
     document.getElementById("youtubelink").style.display = 'block';
 }
 
+var trailer = document.getElementById("trailer");
 
+function trailerplay(){
+    trailer.play(); 
+}
+
+function pause_video(){ 
+    trailer.pause();
+}
