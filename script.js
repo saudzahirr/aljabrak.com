5735ea2daba3c238d359cb04bf308dfbf7df6338
Saud Zahir <76210541+aljabrak@users.noreply.github.com>
2021-11-25 00:01:00 +0500

    Update script.js

--- script.js
+++ script.js
@@ -6,17 +6,3 @@ function youtube()
 {
     document.getElementById("youtubelink").style.display = 'block';
 };
-
-
-var trailer = document.getElementById("trailer");
-
-function trailerplay(){
-    trailer.play(); 
-};
-
-function pause_video(){ 
-    trailer.pause();
-};
-
-
-
