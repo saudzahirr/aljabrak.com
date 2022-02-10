97959f9f08f9a9732f28a6e0e04bb02288d43fca
Saud Zahir <76210541+saudzahirr@users.noreply.github.com>
2022-02-10 19:55:28 +0500

    Create styles.cs

---
+++ styles.cs
@@ -0,0 +1,17 @@
+body {
+    background-color: black;
+  }
+  img {
+      height: 250px;
+      width: 250px;
+      text-align: center;
+  }
+  h1 {
+    color: white;
+    text-align: center;
+  }
+  
+  p {
+    font-family: Georgia;
+    font-size: 200px;
+  }
