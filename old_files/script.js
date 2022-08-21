404c34db132d4bef8fd309b2a2de750035637345
Saud Zahir <mzahir.bee19seecs@seecs.edu.pk>
2022-08-21 07:42:20 +0500

    Delete old_files directory
    Removing all old files.

--- old_files/script.js
+++
@@ -1,8 +0,0 @@
-function quote(){
-    return "If you can't explain it simply you don't understand it well enough."
-};
-
-function youtube()
-{
-    document.getElementById("youtubelink").style.display = 'block';
-};
